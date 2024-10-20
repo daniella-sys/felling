07cdaf1c02807977606b83adcb6e19e1ebeaa915
daniella-sys <hoohhausaufgaben@gmail.com>
2024-10-20 20:56:34 +0300

    Good project for you !

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-print("Hello")
+import calendar
+
+
+def print_calendar(year, month):
+    cal = calendar.month(year, month)
+#Enter year
+year = int(input("Enter please the year!:  "))
+#Enter month
+month = int(input("Enter please the month: "))
+print_calendar(year, month)
+
+
 
 
 
