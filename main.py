ccf38cdada42e4f7f8f5af65c2d4410eb1d29b26
daniella-sys <hoohhausaufgaben@gmail.com>
2024-10-27 12:21:41 +0200

    Work from information.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import calendar
+class Book:
+    def __init__(self, title, author, years, famous):
+        self.title = title
+        self.author = author
+        self.years = years
+        self.famous = famous
+#Виводемо інформацію
+    def display_info(self):
+        print(f"title: {self.title}, author: {self.author}, years: {self.years}, famous: {self.famous}")
+
+#Створення самого класу з даними
+book1 = Book("48 laws of power", "Robert Green", 2021, True)
+book1.display_info()
 
 
-def print_calendar(year, month):
-    cal = calendar.month(year, month)
-#Enter year
-year = int(input("Enter please the year!:  "))
-#Enter month
-month = int(input("Enter please the month: "))
-print_calendar(year, month)
 
 
 
