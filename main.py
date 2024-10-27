bd2287ee5dc4c8fed2e0618331c6af5674bd6543
daniella-sys <hoohhausaufgaben@gmail.com>
2024-10-27 13:34:00 +0200

    Download video from YouTube

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-class Kyiv:
-      def __init__(self, title, years, famous, peoples):
-          self.title = title
-          self.years = years
-          self.famous = famous
-          self.peoples = peoples
-#Enter information
-      def display_info(self):
-          print(f"title: {self.title}, years: {self.years}, famous: {self.famous}, peoples: {self.peoples}")
+from pytube import YouTube
 
-kiew1 = Kyiv("Kyiv", 482, True, "1 milion")
-kiew1.display_info()
+def download_video(url):
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.get_highest_resolution()
+        stream.download()
+        print("Завантаження завершено!")
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
 
-
-
-
-
-
+if __name__ == "__main__":
+    video_url = input("Введіть URL відео з YouTube: ")
+    download_video(video_url)
 
