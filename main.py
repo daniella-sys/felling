45c8bc1b0483161e25a14b558f89cf47dcaa25ae
daniella-sys <hoohhausaufgaben@gmail.com>
2024-11-17 13:26:56 +0200

    progress bar python.

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-import math
-from turtle import *
+from tqdm import tqdm
+import time
 
-def hearta(k):
-    return 15 * math.sin(k)**3
-def heartb(k):
-    return 12*math.cos(k)-5*\
-    math.cos(2*k)-2*\
-    math.cos(3*k)-\
-    math.cos(4*k)
-speed(9000)
-bgcolor("black")
-
-for i in range(6000):
-    goto(hearta(i)*20,heartb(i)*20)
-    for j in range(5):
-        color("red")
-        goto(0,0)
-done()
+def count(n):
+    count = 0
+    for i in tqdm(
+        range(n),
+        desc="Йде підрахунок",
+        ncols=70,
+        colour="#009FBD",
+    ):
+        count += 1
+        time.sleep(0.1)
+    return count
+total = count(20)
+print(f"Всього підраховано: {total}")
