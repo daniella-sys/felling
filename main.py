22dc14937ce1b3660c3da511278bfb1ae05945cf
daniella-sys <hoohhausaufgaben@gmail.com>
2024-12-01 16:40:39 +0200

    translate

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from tqdm import tqdm
-import time
+import requests
 
-def count(n):
-    count = 0
-    for i in tqdm(
-        range(n),
-        desc="Йде підрахунок",
-        ncols=70,
-        colour="#009FBD",
-    ):
-        count += 1
-        time.sleep(0.1)
-    return count
-total = count(20)
-print(f"Всього підраховано: {total}")
+def translate_text(text, target_lang='uk'):
+    url = "https://translate.googleapis.com/translate_a/single"
+    params = {
+        'client': 'gtx',
+        'sl': 'auto',
+        'tl': target_lang,
+        'dt': 't',
+        'q': text
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        translation = response.json()[0][0][0]
+        return translation
+    else:
+        return "Error: Unable to translate text"
+
+# Використання функції перекладу
+text_to_translate = "Hello, world!"
+translated_text = translate_text(text_to_translate)
+print(translated_text)
