4061b111d209fab85bfd94b8582d7b23b73370c2
daniella-sys <hoohhausaufgaben@gmail.com>
2024-12-29 18:27:35 +0200

    generate password Python

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import requests
-from lxml import html
-#Import libraries
-
-#URL Web-pages
-url = 'https://www.bbc.com/news'
-
-#Send me NTML-code pages
-response = requests.get(url) #Отримуємо посилання
-html_content = response.content
-
-#Парсинг NTML за допомогою lxml
-tree = html.fromstring(html_content)
-
-#Знаходження заголовків
-news_headlines = tree.xpath('//h2[@class="news-title"]/text()')
-
-#Вивід заголовків
-for headline in news_headlines:
-    print(headline)
+import random
+import string
+def generate_password(lengh=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters)
+                       for _ in range(lengh))
+    return password
+print(generate_password(12))
